6785ce91677d8d4f0ea661467c0b4e0a008a6eba
Rickvm <rick.van.melis@peercode.nl>
2021-06-23 16:44:00 +0200

    setup truffle

--- truffle-config.js
+++ truffle-config.js
@@ -9,12 +9,17 @@ module.exports = {
         port: 8545,
         network_id: "*" // Match any network id
       },
-      bscTestnet: {
-          host: "",
-          port: "",
-          network_id: ,
-          provider:
-      }
+      testnet: {
+        provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+        network_id: 97,
+        confirmations: 10,
+        timeoutBlocks: 200,
+        skipDryRun: true
+      },
+    },
+    // Set default mocha options here, use special reporters etc.
+    mocha: {
+        // timeout: 100000
     },
     compilers: {
       solc: {
