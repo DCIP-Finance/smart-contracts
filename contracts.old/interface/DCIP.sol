c7a89d49dc6388af5edd32197ac094e84a010dc4
Jasper Verbeet <jasper@worldofcontent.com>
2021-06-24 17:02:32 +0200

    Deletes unused files

--- contracts.old/interface/DCIP.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Unlicensed
-
-pragma solidity >=0.6.8;
-
-interface DCIP {
-    function transfer(address to, uint256 amount) external;
-
-    function balanceOf(address account) external view returns (uint256);
-}
